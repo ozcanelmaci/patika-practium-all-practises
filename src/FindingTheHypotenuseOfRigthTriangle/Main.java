76a30bbd1af21fd519e29c9a1687ad0b663ee0ce
Özcan Elmacı <ozcan.elmaci@ogr.deu.edu.tr>
2022-11-04 17:23:52 +0300

    Finding The Area Of Triangle Practice has been completed successfully.

--- src/FindingTheHypotenuseOfRigthTriangle/Main.java
+++ src/FindingTheHypotenuseOfRigthTriangle/Main.java
@@ -5,22 +5,35 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        double firstPerpendicularEdge, secondPerpendicularEdge, hypotenuse, perimeterOfTriangle, areaOfTriangle, u;
+        //ÖNEMLİ NOT : hipotenüs bulma kodu için yorum satırındaki kodu, 3 kenar uzunluğunu alıp alan bulma kodu için programı direkt çalıştırın.
 
-        System.out.print("Birinci Dik Kenar Uzunluğu : ");
-        firstPerpendicularEdge = scn.nextDouble();
+//        double firstPerpendicularEdge, secondPerpendicularEdge, hypotenuse;
+//
+//        System.out.print("Birinci Dik Kenar Uzunluğu : ");
+//        firstPerpendicularEdge = scn.nextDouble();
+//
+//        System.out.print("İkinci Dik Kenar Uzunluğu : ");
+//        secondPerpendicularEdge = scn.nextDouble();
+//
+//        hypotenuse = Math.sqrt(Math.pow(firstPerpendicularEdge,2) + Math.pow(secondPerpendicularEdge,2));
+//        System.out.println("Hipotenüs : " + hypotenuse);
 
-        System.out.print("İkinci Dik Kenar Uzunluğu : ");
-        secondPerpendicularEdge = scn.nextDouble();
+        double firstEdge, secondEdge, thirdEdge, perimeterOfTriangle, areaOfTriangle, u;
 
-        hypotenuse = Math.sqrt(Math.pow(firstPerpendicularEdge,2) + Math.pow(secondPerpendicularEdge,2));
-        System.out.println("Hipotenüs : " + hypotenuse);
+        System.out.print("Birinci Kenar Uzunluğu : ");
+        firstEdge = scn.nextDouble();
 
-        perimeterOfTriangle = firstPerpendicularEdge + secondPerpendicularEdge + hypotenuse;
+        System.out.print("İkinci Kenar Uzunluğu : ");
+        secondEdge = scn.nextDouble();
+
+        System.out.print("Üçüncü Kenar Uzunluğu : ");
+        thirdEdge = scn.nextDouble();
+
+        perimeterOfTriangle = firstEdge + secondEdge + thirdEdge;
         System.out.println("Üçgenin Çevresi : " + perimeterOfTriangle);
 
         u = perimeterOfTriangle / 2;//the half of perimeter
-        areaOfTriangle = Math.sqrt(u * (u-firstPerpendicularEdge) * (u-secondPerpendicularEdge) * (u-hypotenuse));
+        areaOfTriangle = Math.sqrt(u * (u-firstEdge) * (u-secondEdge) * (u-thirdEdge));
         System.out.println("Üçgenin Alanı : " + areaOfTriangle);
     }
 }
