1a54d5ead15b9076251628634faf1020dce5ac17
Özcan Elmacı <ozcan.elmaci@ogr.deu.edu.tr>
2022-11-04 15:40:22 +0300

    KDV Cost Calculator Practice has been completed successfully.

--- src/KDVCostCalculator/Main.java
+++ src/KDVCostCalculator/Main.java
@@ -5,16 +5,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int kdvPercent, costWithoutKDV;
-        double costWithKDV, costOfKDV;
+        double costWithoutKDV, costWithKDV, costOfKDV;
+        int kdvPercent;
 
         System.out.print("Tutarı girin: ");
-        costWithoutKDV = scn.nextInt();
+        costWithoutKDV = scn.nextDouble();
         kdvPercent = costWithoutKDV > 1000 ? 8 : 18;
-        costOfKDV = (double)(costWithoutKDV * kdvPercent) / 100;
-        costWithKDV = (double)(costWithoutKDV) + costOfKDV;
+        costOfKDV = costWithoutKDV * kdvPercent / 100;
+        costWithKDV = costWithoutKDV + costOfKDV;
 
         System.out.println("KDV'siz Fiyat = " + costWithoutKDV);
+        System.out.println("KDV Oranı = %" + kdvPercent);
         System.out.println("KDV'li Fiyat = " + costWithKDV);
         System.out.println("KDV Tutarı = " + costOfKDV);
     }
