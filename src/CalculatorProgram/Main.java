f01f765e9e2fa54f76d915cf537e5f1d2877f12c
Özcan Elmacı <ozcan.elmaci@ogr.deu.edu.tr>
2022-11-10 23:54:48 +0300

    Calculator Program Practice has been completed successfully.

---
+++ src/CalculatorProgram/Main.java
@@ -0,0 +1,60 @@
+package CalculatorProgram;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int firstNumber, secondNumber, selection;
+
+        System.out.print("İlk sayıyı giriniz: ");
+        firstNumber = scn.nextInt();
+        System.out.print("İkinci sayıyı giriniz: ");
+        secondNumber = scn.nextInt();
+
+        System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
+        System.out.print("Seçiminiz: ");
+        selection = scn.nextInt();
+
+        switch (selection){
+            case 1:
+                System.out.println("Toplam : " + sum(firstNumber,secondNumber));
+                break;
+            case 2:
+                System.out.println("Çıkarma : " + subtract(firstNumber, secondNumber));
+                break;
+            case 3:
+                System.out.println("Çarpma : " + multiply(firstNumber,secondNumber));
+                break;
+            case 4:
+                if(secondNumber == 0){
+                    System.out.println("Sayılar 0'a bölünemez!!!");
+                }else{
+                    System.out.println("Bölme : " + divide(firstNumber,secondNumber));
+                }
+                break;
+            default:
+                System.out.println("Hatalı seçim yaptınız!!!");
+        }
+    }
+
+    public static int sum(int firstNumber, int secondNumber){
+        int sum = firstNumber + secondNumber;
+        return sum;
+    }
+
+    public static int subtract(int firstNumber, int secondNumber){
+        int subtract = firstNumber - secondNumber;
+        return subtract;
+    }
+
+    public static int multiply(int firstNumber, int secondNumber){
+        int multiply = firstNumber * secondNumber;
+        return  multiply;
+    }
+
+    public static double divide(int firstNumber, int secondNumber){
+        double divide = (double) firstNumber / secondNumber;
+        return divide;
+    }
+}
