a906e893ef6781c2fd33abe871b23e8371e28caf
Özcan Elmacı <ozcan.elmaci@ogr.deu.edu.tr>
2022-11-11 00:51:42 +0300

    User Entrance Password Changer Practice has been completed successfully.

---
+++ src/UserEntrance/Main.java
@@ -0,0 +1,48 @@
+package UserEntrance;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String userName, password;
+        char answer;
+        int counter = 0;
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Kullanıcı Adınız : ");
+        userName = scn.nextLine();
+
+        System.out.print("Şifreniz : ");
+        password = scn.nextLine();
+
+        if(userName.equals("patika") && password.equals("12345")){
+            System.out.println("Başarılı bir şekilde giriş yapıldı.");
+        }else {
+            System.out.print("Şifrenizi sıfırlamak ister misiniz? Y/N : ");
+            answer = scn.nextLine().toUpperCase().charAt(0);
+            if (answer == 'Y'){
+                String newPassword = "12345";
+                while (newPassword.equals("12345")){
+                    counter++;
+                    if(counter == 4){
+                        break;
+                    }
+                    if(counter != 1){
+                        System.out.println("Şifreniz unuttuğunuz şifre ile aynı olamaz!!!");
+                    }
+                    System.out.print("Yeni Şifrenizi Giriniz : ");
+                    newPassword = scn.nextLine();
+                }
+                if(counter == 4){
+                    System.out.println("5 kere hatalı şifre oluşturmayı denediniz!!!");
+                }else {
+                    System.out.println("Şifre oluşturuldu.");
+                }
+
+            }else{
+                System.out.println("Şifreniz eskisi gibi kalacaktır!!!");
+            }
+        }
+    }
+}
